f9e02cd2c45834ae393b3f10f5ef0635a6660395
Rens van Driel <rens.van.a.driel@sogeti.com>
2022-04-19 13:31:55 +0200

    added borders data, general styling

--- main.js
+++ main.js
@@ -2,16 +2,29 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
 
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
   view: new View({
     center: [0, 0],
     zoom: 2
   })
 });
+
+const standardMapLayer = new TileLayer({     // Creates standardmap layer/
+  source: new OSM(),
+});
+standardMapLayer.setZIndex(0);
+map.addLayer(standardMapLayer);     // Adds basic map to map object.
+
+const countryBorderLayer = new VectorLayer({  // Creates new VectorLayer with country borders.
+  source: new VectorSource({
+    format: new GeoJSON(),
+    url: './data/countries.json',
+  })
+});
+countryBorderLayer.setZIndex(2);
+map.addLayer(countryBorderLayer);   // Adds country border layer to map object.
